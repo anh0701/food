31114767f8f692c6e438fbcf6698c583a69e02a6
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2024-12-22 14:56:49 +0700

    modified sign up page: input birthday

--- food-supply/src/app/sign-up/page.tsx
+++ food-supply/src/app/sign-up/page.tsx
@@ -3,7 +3,7 @@ import { Bitter } from "next/font/google";
 import { features } from "process";
 import React, { useState } from "react";
 
-export default function SignUp(){
+export default function SignUp() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPass, setConfirmPass] = useState('');
@@ -13,19 +13,19 @@ export default function SignUp(){
     const regexUsername = /^[A-Za-z]\w{5,29}$/;
 
     const handleSignUp = async () => {
-        if(!regexPass.test(password)){
+        if (!regexPass.test(password)) {
             alert("Please enter password must be ....")
         }
-        if(!regexUsername.test(username)){
+        if (!regexUsername.test(username)) {
             alert("Username must be ...");
         }
-        if(password !== confirmPass){
+        if (password !== confirmPass) {
             alert("password and confirm password must be the same...")
         }
 
-        const response = await fetch( "", {
+        const response = await fetch("", {
             method: "POST",
-            body: JSON.stringify({username, password, name, birthday})
+            body: JSON.stringify({ username, password, name, birthday })
         });
         const data = await response.json();
         console.log(data);
@@ -51,10 +51,16 @@ export default function SignUp(){
         <div className="w-screen h-screen flex flex-col justify-center items-center bg-blue-400">
             <div className="bg-white rounded-2xl flex flex-col justify-center items-center p-8 g-2 shadow-md">
                 <div className="font-bold text-2xl mb-4">Sign up</div>
-                <input onChange={handleNameChange} type="text" placeholder="Full name" className="rounded-md w-full bg-slate-200 mb-4 p-2"/>
+                <input onChange={handleNameChange} type="text" placeholder="Full name" className="rounded-md w-full bg-slate-200 mb-4 p-2" />
                 <input onChange={handlePasswordChange} type="password" placeholder="Password" className="rounded-md w-full bg-slate-200 mb-4 p-2" />
                 <input type="password" placeholder="Confirm password" className="rounded-md w-full bg-slate-200 mb-4 p-2" />
-                <input onChange={handleBirthdayChange} type="date" name="" id="rounded-md w-full bg-slate-200 mb-4 p-2" />
+
+                <div className="w-full">
+                    <label className=" block text-sm font-semibold">Birthday</label>
+                    <input onChange={handleBirthdayChange} type="date" name="date" className="rounded-md w-full bg-slate-200 mb-4 p-2" />
+                </div>
+
+
                 <button onClick={handleSignUp} type="button" className="w-full bg-blue-400 rounded-md text-white hover:bg-blue-500 transition-colors duration-300 p-2">Submit</button>
             </div>
         </div>
