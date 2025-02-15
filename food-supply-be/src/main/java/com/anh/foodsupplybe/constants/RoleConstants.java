cf9ff6a77b9b0d3511dddb11db901a47de448ad7
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2025-02-15 21:35:39 +0700

    modified structure

--- food-supply-be/src/main/java/com/anh/foodsupplybe/constants/RoleConstants.java
+++
@@ -1,7 +0,0 @@
-package com.anh.foodsupplybe.constants;
-
-public interface RoleConstants {
-    String ROLE_USER = "USER";
-    String ROLE_ADMIN = "ADMIN";
-    String ROLE_GUEST = "GUEST";
-}
--- food-supply-be/src/main/java/com/anh/foodsupplybe/constants/StatusConstants.java
+++
@@ -1,6 +0,0 @@
-package com.anh.foodsupplybe.constants;
-
-public interface StatusConstants {
-    String ACTIVE = "ACTIVE";
-    String INACTIVE = "INACTIVE";
-}
